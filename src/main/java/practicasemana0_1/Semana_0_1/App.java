8c19dc3d73afc3fc38c922a514b8c0dab177ecd0
alejandroupc <alejandroupc@AlejandroUPC-Ubuntu>
2015-02-21 21:36:18 +0100

    First commit

---
+++ src/main/java/practicasemana0_1/Semana_0_1/App.java
@@ -0,0 +1,13 @@
+package practicasemana0_1.Semana_0_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
